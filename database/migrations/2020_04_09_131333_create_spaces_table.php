6f6c806a51c3c472fd77bc6cb8f14730eb98d137
Syaiful Romadhon <sngeblog@gmail.com>
2020-04-09 20:20:32 +0700

    initial commit

---
+++ database/migrations/2020_04_09_131333_create_spaces_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spaces', function (Blueprint $table) {
+            $table->id();
+            $table->UnsignedBigInteger('user_id');
+            $table->string('title');
+            $table->text('address');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spaces');
+    }
+}
